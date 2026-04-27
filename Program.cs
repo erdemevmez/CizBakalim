cb7c8a7db81c2cccf34643dddd76c94c3651bf25
erdemevmez <erdemevmez.com@gmail.com>
2026-04-27 13:51:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using CizBakalım.Data;
+using CizBakalım.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// SignalR
+builder.Services.AddSignalR();
+
+// Session (kimlik doğrulama için)
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(2);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.Cookie.Name = ".CizBakalim.Session";
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<DrawingHub>("/drawingHub");
+
+app.Run();
